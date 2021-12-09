1edaf1505f6c73f2fa048317a7bf452095a4c73f
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2021-12-09 08:49:55 +0100

    merge algo implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
-pub fn sort(array: &Vec<i32>) -> &Vec<i32> {
-    array
+pub fn merge(mut slice1: Vec<i32>, mut slice2: Vec<i32>) -> Vec<i32> {
+    let height: i32 = (slice1.len() + slice2.len()) as i32;
+    let mut count: i32 = 0;
+    let mut result: Vec<i32> = vec![];
+    while count < height {
+        if slice1.len() != 0 && slice2.len() != 0 {
+            if slice1[0] < slice2[0] {
+                result.push(slice1[0]);
+                slice1.remove(0);
+            } else if slice1[0] > slice2[0] {
+                result.push(slice2[0]);
+                slice2.remove(0);
+            } else if slice1[0] == slice2[0] {
+                result.push(slice1[0]);
+                result.push(slice2[0]);
+                slice1.remove(0);
+                slice2.remove(0);
+            }
+        } else if slice1.len() != 0 && slice2.len() == 0 {
+            result.push(slice1[0]);
+            slice1.remove(0);
+        } else if slice1.len() == 0 && slice2.len() != 0 {
+            result.push(slice2[0]);
+            slice2.remove(0);
+        }
+        count += 1;
+    }
+    result
 }
+
+/*pub fn sort(mut array: Vec<i32>) -> Vec<i32> {
+    println!("{:?}", array);
+    if array.len() >= 2 {
+        let (slice1, slice2): (Vec<i32>, Vec<i32>) = array
+            .clone()
+            .into_iter()
+            .partition(|x| x % 2 ==0);
+        array[0] = slice1[0];
+        array[1] = slice2[0];
+        println!("Slice 1: {:?}, Slice 2: {:?}", slice1, slice2);
+    }
+    array
+}*/
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,12 +1,70 @@
 #[cfg(test)]
 mod tests {
-    use merge_sort_tdd::sort;
+    use merge_sort_tdd::merge;
 
     #[test]
-    fn it_works_test() {
-        let v: Vec<i32> = vec![];
-        let expected: Vec<i32> = vec![];
-        assert_eq!(sort(&v), &expected);
+    fn it_merge_two_vec() {
+        let slice1: Vec<i32> = vec![2, 5];
+        let slice2: Vec<i32> = vec![6, 7];
+        let expected: Vec<i32> = vec![2, 5, 6, 7];
+        assert_eq!(expected, merge(slice1, slice2));
     }
 
+    #[test]
+    fn it_merge_two_vec_in_different_order() {
+        let slice1: Vec<i32> = vec![5, 8];
+        let slice2: Vec<i32> = vec![2, 6];
+        let expected: Vec<i32> = vec![2, 5, 6, 8];
+        assert_eq!(expected, merge(slice1, slice2));
+    }
+
+    #[test]
+    fn it_merge_two_vec_with_same_numbers() {
+        let slice1: Vec<i32> = vec![2, 5];
+        let slice2: Vec<i32> = vec![2, 5];
+        let expected: Vec<i32> = vec![2, 2, 5, 5];
+        assert_eq!(expected, merge(slice1, slice2));
+    }
+
+    /*#[test]
+    fn it_sort_empty() {
+        let v: Vec<i32> = vec![];
+        let expected: Vec<i32> = vec![];
+        assert_eq!(expected, sort(v));
+    }
+
+    #[test]
+    fn it_sort_one_item() {
+        let v: Vec<i32> = vec![1];
+        let expected: Vec<i32> = vec![1];
+        assert_eq!(expected, sort(v));
+    }
+
+    #[test]
+    fn it_sort_two_identical_items() {
+        let v: Vec<i32> = vec![2, 5];
+        let expected: Vec<i32> = vec![2, 5];
+        assert_eq!(expected, sort(v));
+    }
+
+    #[test]
+    fn it_sort_two_items() {
+        let v: Vec<i32> = vec![5, 2];
+        let expected: Vec<i32> = vec![2, 5];
+        assert_eq!(expected, sort(v));
+    }
+
+    #[test]
+    fn it_sort_three_items() {
+        let v: Vec<i32> = vec![1, 5, 2];
+        let expected: Vec<i32> = vec![1, 2, 5];
+        assert_eq!(expected, merge(v));
+    }
+
+    #[test]
+    fn it_sort_three_others_items() {
+        let v: Vec<i32> = vec![5, 1, 2];
+        let expected: Vec<i32> = vec![1, 2, 5];
+        assert_eq!(expected, merge(v));
+    }*/
 }
