60e5dd314c6367aef3bae1a135ca7e8b0f2fb0ac
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2021-12-15 13:59:08 +0100

    use for instead while and count

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 pub fn merge(mut slice1: Vec<i32>, mut slice2: Vec<i32>) -> Vec<i32> {
     let height: i32 = (slice1.len() + slice2.len()) as i32;
-    let mut count: i32 = 0;
     let mut result: Vec<i32> = vec![];
-    while count < height {
+    for _ in 0..height {
         if slice1.len() != 0 && slice2.len() != 0 {
             if slice1[0] < slice2[0] {
                 result.push(slice1[0]);
@@ -23,14 +22,13 @@ pub fn merge(mut slice1: Vec<i32>, mut slice2: Vec<i32>) -> Vec<i32> {
             result.push(slice2[0]);
             slice2.remove(0);
         }
-        count += 1;
     }
     result
 }
 
 pub fn divide(array: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
-    let mid = array.len()/2;
-    let last = array.len();
+    let mid: usize = array.len()/2;
+    let last: usize = array.len();
     let mut slice1: Vec<i32> = vec![];
     let mut slice2: Vec<i32> = vec![];
     slice1.append(&mut array[0..mid].to_vec());
@@ -41,7 +39,7 @@ pub fn divide(array: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
 pub fn sort(mut array: Vec<i32>) -> Vec<i32>  {
     if array.len() > 1 {
         if array[0] > array[1] {
-            let tmp = array[0];
+            let tmp: i32 = array[0];
             array[0] = array[1];
             array[1] = tmp;
         }
